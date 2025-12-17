5743d249e63bc1fe6a8ed72792894c8f61349b35
Justin George <justin.george@gmail.com>
2025-12-16 23:17:04 -0800

    stub options for .lqs generation

--- interpreter/src/Main.hs
+++ interpreter/src/Main.hs
@@ -12,12 +12,30 @@ import           Parser
 main :: IO ()
 main = do
   args <- getArgs
-  let file = case args of
-        []    -> "../examples/00_hello_world.lqs"
-        (x:_) -> x
+  case args of
+    ["--help"] -> putStrLn usage
+    ["--run-lqs", file] -> runLqs file
+    ["--run-lq", file] -> die "M-expression (.lq) parsing not implemented yet."
+    ["--emit-lqs", file, out] -> die "Conversion from .lq to .lqs not implemented yet."
+    [] -> runLqs "../examples/00_hello_world.lqs"
+    [file] -> runLqs file
+    _ -> die usage
+
+runLqs :: FilePath -> IO ()
+runLqs file = do
   cwd <- getCurrentDirectory
   let projectRoot = takeDirectory cwd
   contents <- T.readFile file
   case parseModuleFile file contents of
     Left err -> die err
     Right m  -> runModuleMain projectRoot m
+
+usage :: String
+usage = unlines
+  [ "locque-interpreter usage:"
+  , "  locque-interpreter [--run-lqs <file>]"
+  , "  locque-interpreter --run-lq <file>            (not yet implemented)"
+  , "  locque-interpreter --emit-lqs <file> <out>    (not yet implemented)"
+  , "  locque-interpreter --help"
+  , "Default: run ../examples/00_hello_world.lqs"
+  ]
